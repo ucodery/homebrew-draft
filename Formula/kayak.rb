651c4efaa521c3e14de03368171953583a02c7b9
Jeremy Paige <ucodery@gmail.com>
2024-02-26 13:51:58 -0800

    manual lint fix

--- Formula/kayak.rb
+++ Formula/kayak.rb
@@ -1,38 +1,26 @@
 class Kayak < Formula
+  desc "You ought to know about your artifact's key-data"
+  homepage "https://github.com/ucodery/kayak"
   version "0.5.0"
-  on_macos do
-    on_arm do
+  if OS.mac?
+    if Hardware::CPU.arm?
       url "https://github.com/ucodery/kayak/releases/download/v0.5.0/kayak-aarch64-apple-darwin.tar.xz"
       sha256 "f733a7f47e011ec4f664f0aee02e295ac9e7039ad3610abd7c749583c188d0cc"
     end
-    on_intel do
+    if Hardware::CPU.intel?
       url "https://github.com/ucodery/kayak/releases/download/v0.5.0/kayak-x86_64-apple-darwin.tar.xz"
       sha256 "45440cf17b08c689e7f36b0b58c8c98c0ed4f1ac1b7972815470f287b1db6eca"
     end
   end
-  on_linux do
-    on_intel do
-      url "https://github.com/ucodery/kayak/releases/download/v0.5.0/kayak-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "74716db62d6798a47f039fa9753a13084f992ee3b3137cd32e4f4e71eee6ffe5"
-    end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/ucodery/kayak/releases/download/v0.5.0/kayak-x86_64-unknown-linux-gnu.tar.xz"
+    sha256 "74716db62d6798a47f039fa9753a13084f992ee3b3137cd32e4f4e71eee6ffe5"
   end
 
   def install
-    on_macos do
-      on_arm do
-        bin.install "kayak"
-      end
-    end
-    on_macos do
-      on_intel do
-        bin.install "kayak"
-      end
-    end
-    on_linux do
-      on_intel do
-        bin.install "kayak"
-      end
-    end
+    bin.install "kayak" if OS.mac? && Hardware::CPU.arm?
+    bin.install "kayak" if OS.mac? && Hardware::CPU.intel?
+    bin.install "kayak" if OS.linux? && Hardware::CPU.intel?
 
     # Homebrew will automatically install these, so we don't need to do that
     doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
@@ -40,6 +28,6 @@ class Kayak < Formula
 
     # Install any leftover files in pkgshare; these are probably config or
     # sample files.
-    pkgshare.install *leftover_contents unless leftover_contents.empty?
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
   end
 end
